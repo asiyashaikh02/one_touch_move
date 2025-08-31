eaec92678da55298a1eb95e298a2f7c0d3e56dd2
asiyashaikh02 <shkasiya02@gmail.com>
2025-08-31 18:00:50 +0530

    Fix public images path

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,27 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Homepage from "./homepage";
 import About from "./about";
 import Services from './services';
 import RegisterPhysio from './RegisterPhysio';
 import Signup from "./signup";
 import Login from "./login";
+
 function App() {
   return (
-    
     <Router>
       <Routes>
         <Route path="/" element={<Homepage />} />
-        
         <Route path="/about" element={<About />} />
         <Route path="/services" element={<Services />} />
         <Route path="/register-physio" element={<RegisterPhysio />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
 
+        {/* 🔥 Catch-all route */}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
-
-
